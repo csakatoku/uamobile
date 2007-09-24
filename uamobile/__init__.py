94085ab484c3bf2f46aa8beaeac54dc0f92925e5
csakatoku <csakatoku@8f09c748-102d-0410-94f3-bb0d8cc1b7f9>
2007-09-24 02:14:54 +0000

    r902@IBM-01053B5C663:  csakatoku | 2007-09-23  東京 (標準時)  - Refactored.
    git-svn-id: http://wsgiuseragentmobile.googlecode.com/svn/trunk@34 8f09c748-102d-0410-94f3-bb0d8cc1b7f9

--- uamobile/__init__.py
+++ uamobile/__init__.py
@@ -9,50 +9,36 @@ from uamobile.willcom import WillcomUserAgent as Willcom
 
 __all__ = ['detect', 'NonMobile', 'DoCoMo', 'EZweb', 'SoftBank', 'Willcom']
 
-DOCOMO_RE = r'^DoCoMo/\d\.\d[ /]'
-SOFTBANK_RE = r'^(?:(?:SoftBank|Vodafone|J-PHONE)/\d\.\d|MOT-)'
-EZWEB_RE = r'^(?:KDDI-[A-Z]+\d+[A-Z]? )?UP\.Browser\/'
-WILLCOM_RE = r'^Mozilla/3\.0\((?:DDIPOCKET|WILLCOM);'
+DOCOMO_RE   = re.compile(r'^DoCoMo/\d\.\d[ /]')
+SOFTBANK_RE = re.compile(r'^(?:(?:SoftBank|Vodafone|J-PHONE)/\d\.\d|MOT-)')
+EZWEB_RE    = re.compile(r'^(?:KDDI-[A-Z]+\d+[A-Z]? )?UP\.Browser\/')
+WILLCOM_RE  = re.compile(r'^Mozilla/3\.0\((?:DDIPOCKET|WILLCOM);|^Mozilla/4\.0 \(compatible; MSIE (?:6\.0|4\.01); Windows CE; SHARP/WS\d+SH; PPC; \d+x\d+\)')
 
-MOBILE_RE = re.compile(r'(?:(%s)|(%s)|(%s)|(%s))' % (DOCOMO_RE,
-                                                     SOFTBANK_RE,
-                                                     EZWEB_RE,
-                                                     WILLCOM_RE))
-
-WILLCOM_WZERO3_RE = re.compile(r'^Mozilla/4\.0 \(compatible; MSIE (?:6\.0|4\.01); Windows CE; SHARP/WS\d+SH; PPC; \d+x\d+\)')
-
-def detect(environ, lazy=False):
+def detect(environ):
     """
-    parse HTTP user agent string and detect a mobile device.   
+    parse HTTP user agent string and detect a mobile device.
     """
     try:
         useragent = environ['HTTP_USER_AGENT']
-        matcher = MOBILE_RE.match(useragent)
-        if matcher:
-            g = matcher.groups()
-            if g[0]:
-                result = DoCoMo(environ)
-            elif g[1]:
-                result = SoftBank(environ)
-            elif g[2]:
-                result = EZweb(environ)
-            else:
-                result = Willcom(environ)
-        else:
-            if WILLCOM_WZERO3_RE.match(useragent):
-                result = Willcom(environ)
-            else:
-                result = NonMobile(environ)
-
     except KeyError:
-        result = NonMobile(environ)
+        useragent = ''
 
-    # Currently we does not support lazy evaluation.
+    if DOCOMO_RE.match(useragent):
+        device = DoCoMo(environ)
+    elif EZWEB_RE.match(useragent):
+        device = EZweb(environ)
+    elif SOFTBANK_RE.match(useragent):
+        device = SoftBank(environ)
+    elif WILLCOM_RE.match(useragent):
+        device = Willcom(environ)
+    else:
+        device = NonMobile(environ)
+
     try:
-        result.parse()
+        device.parse()
     except exceptions.NoMatchingError:
         raise
     #except ValueError, e:
     #    raise exceptions.NoMatchingError(result, e)
-    
-    return result
+
+    return device
